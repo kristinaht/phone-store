5b7c0e7809b58231c66e3596a1c81c4c9ab5481d
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-06-02 16:55:37 -0700

    initial commit including css

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-6'>column number one</div>
+        <div className='col-6'><span><i className="fa fa-home"></i></span></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
